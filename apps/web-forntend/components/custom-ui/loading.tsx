066be44245c0c57ccac871f88863ca2715bab914
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-11-02 17:32:19 +0530

    Add Loading Room

--- apps/web-forntend/components/custom-ui/loading.tsx
+++ apps/web-forntend/components/custom-ui/loading.tsx
@@ -1,7 +1,9 @@
+import { Loader } from "lucide-react";
+
 export function Loadingroom(){
     return(
-        <div>
-            <p>Geting your Rooom</p>
+        <div className="min-h-screen flex justify-center items-center">
+            <p className="text-[16px] font-bold text-neutral-500 flex items-center gap-2"> Joining to your room  <Loader className="animate-spin size-4" /> </p>
         </div>
     )
 }
